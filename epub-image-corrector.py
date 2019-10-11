9e0fd334cc81fb2b00cf4916f104baa35d67be3a
Flamy <27062841+f1amy@users.noreply.github.com>
2019-10-11 23:18:41 +0500

    reformat & refactor

--- epub-image-corrector.py
+++ epub-image-corrector.py
@@ -57,7 +57,7 @@ def process_file(path: Path) -> int:
         if not is_zipfile(path):
             print(f"\nerror: {path} "
                   "is not a zip file, skip.")
-            return
+            return 0
         with ZipFile(path) as archive:
             archive.extractall(root_dir)
         mimetype = Path(f"{root_dir}/mimetype")
@@ -65,18 +65,18 @@ def process_file(path: Path) -> int:
             if mimetype.read_text() != 'application/epub+zip':
                 print(f"\nerror: {path} "
                       "is not a application/epub+zip file, skip.")
-                return
+                return 0
         else:
             print(f"\nerror: {path} "
                   "cannot check mimetype, file is "
                   "probably corrupted, skip.")
-            return
+            return 0
         files_changed = 0
         image_extensions = ['jpg', 'jpeg', 'png']
         for extension in image_extensions:
             for file in root_dir.glob(f"OEBPS/images/*.{extension}"):
                 with Image(filename=file) as img:
-                    if (img.profiles['ICC'] == None
+                    if (img.profiles['ICC'] is None
                             and img.colorspace == 'cmyk'
                             or img.colorspace == 'cmyk'
                             and args.force):
@@ -102,12 +102,12 @@ if __name__ == '__main__':
                         help='Path to .icc CMYK profile.')
     parser.add_argument('path', type=file_or_dir,
                         help='Path to .epub file or directory '
-                        'that contains ePub files.')
+                             'that contains ePub files.')
     parser.add_argument('-r', '--recursive', action='store_true',
                         help='Recursive into subdirectories.')
     parser.add_argument('-f', '--force', action='store_true',
                         help='Force replace color profile for '
-                        'CMYK color space images.')
+                             'CMYK color space images.')
 
     args = parser.parse_args()
 
@@ -120,7 +120,7 @@ if __name__ == '__main__':
 
     if work_path.is_file():
         print('Correcting 1 file...')
-        changed_images = process_file(work_path)
+        changed_images = process_file(work_path) or 0
         files = 1 if changed_images > 0 else 0
     else:
         work_files = list(work_path.glob(
@@ -128,7 +128,7 @@ if __name__ == '__main__':
 
         bar = Bar('Processing files:', max=len(work_files),
                   suffix='%(index)d/%(max)d (%(percent)d%%) - '
-                  '[%(eta_td)s / %(elapsed_td)s]')
+                         '[%(eta_td)s / %(elapsed_td)s]')
 
         for file in work_files:
             bar.next()
@@ -144,4 +144,4 @@ if __name__ == '__main__':
     print(f"Corrected {changed_images} "
           f"{'images' if changed_images > 1 else 'image'} "
           f"inside {files} {'files' if files > 1 else 'file'} "
-          f"in {'%.1f'%(elapsed_time)}s.")
+          f"in {'%.1f' % elapsed_time}s.")
